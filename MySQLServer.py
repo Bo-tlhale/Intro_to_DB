3a20d355185b43507d9727f5235bd658ee8af3b0
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-10-22 10:32:49 +0200

    Fixed logic so py script error matches with checker expectations

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 try:
     mydb = mysql.connector.connect(
@@ -13,7 +12,7 @@ try:
         
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
+except mysql.connector.Error as e:
     print(f"Error while connecting to MySQL: {e}")
 
 finally:
